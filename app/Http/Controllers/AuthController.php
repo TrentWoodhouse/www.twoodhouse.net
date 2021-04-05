416fe5b1a75c82d1edc40658f85013e655858021
Trent Woodhouse <trentrwoodhouse@gmail.com>
2021-04-05 08:39:05 +0000

    Removed unneeded line

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
